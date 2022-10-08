a6913b3495d10906feeb8cf4a770bd3d69ed91d9
Pradeep Lobo <pradeep.lobo@sensorup.com>
2022-10-07 19:40:57 -0600

    Move Remote FeedLoader to a separate production folder Feed API.

--- Essential/Essential/Feed API/RemoteFeedLoader.swift
+++ Essential/Essential/Feed API/RemoteFeedLoader.swift
@@ -7,22 +7,20 @@
 
 import Foundation
 
-protocol HTTPClient {
+public protocol HTTPClient {
     func get(from url: URL)
 }
 
-class RemoteFeedLoader {
-    let client: HTTPClient
-    let url: URL
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
     
-    init(url: URL, client: HTTPClient) {
+    public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
     
-    func load() {
+    public func load() {
         client.get(from: url)
     }
 }
-
-
