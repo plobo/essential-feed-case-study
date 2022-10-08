d5c599bfa105538b09eb0494d23514fd2645b09e
Pradeep Lobo <pradeep.lobo@sensorup.com>
2022-10-07 20:04:29 -0600

    Simplify HTTPCLientSpy with array of requestURLs array to verify

--- Essential/EssentialTests/RemoteFeedLoaderTests.swift
+++ Essential/EssentialTests/RemoteFeedLoaderTests.swift
@@ -12,7 +12,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
-        XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     func test_load_requestsDataFromURL() {
@@ -20,7 +20,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let (sut, client) = makeSUT(url: url)
         
         sut.load()
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLs, [url])
     }
     
     func test_loadTwice_requestsDataFromURLTwice() {
@@ -43,11 +43,9 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
         var requestedURLs: [URL] = []
         
         func get(from url: URL) {
-            requestedURL = url
             requestedURLs.append(url)
         }
         
