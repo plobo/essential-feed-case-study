398bce6fdfaf02a8e8e931d3f3eb859d5ba755a7
Pradeep Lobo <pradeep.lobo@sensorup.com>
2022-10-20 20:19:37 -0600

    Ensure that completion handler in RemoteFeedLoader does not get called after sut instance is deallocated.

--- Essential/EssentialFrameworkTests/RemoteFeedLoaderTests.swift
+++ Essential/EssentialFrameworkTests/RemoteFeedLoaderTests.swift
@@ -90,6 +90,22 @@ final class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+        let url = URL(string: "http://any-url.com")!
+        let client = HTTPClientSpy()
+        var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
+        
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut?.load(completion: {
+            capturedResults.append($0)
+        })
+        
+        sut = nil
+        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
+        
+        XCTAssertTrue(capturedResults.isEmpty)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!, file: StaticString = #filePath,  line: UInt = #line) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
